be208ebedb4749a8c2b1b8b4f185b09600794882
Bluerose101 <thanhhong10194@gmail.com>
2022-05-18 23:08:17 -0400

    add loop

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+
+let playerScore = 0;
+let computerScore = 0;
+while (playerScore <= 5 || computerScore <= 5 ) {
+//Getting player's choice
+const playChoice = prompt("Rock, paper, or scissors?"); 
+console.log(playChoice);
+
 //function to generate computer's choice
 const computerChoice = computerPlay();
 function computerPlay() {
@@ -12,36 +20,38 @@ function computerPlay() {
         return "scissors";
     }
 }
-console.log(computerChoice);
-
-//Getting player's choice
-const playChoice = prompt("Rock, paper, or scissors?"); 
-console.log(playChoice);
+console.log("Computer's choice is ",computerChoice);
 
  //function to play the game
 function playRound (playChoice, computerChoice) {
     let log = "try it";
     if (playChoice === "rock") {
         if (computerChoice === "paper") {
-            log = "Sorry, you lose! Paper beats Rock";
+            log = "Sorry, you lose! Paper beats Rock"; 
+            computerScore++;
         } else if (computerChoice === "scissors") {
             log = " Yayyyy! you win, Rock beat Scissors";
-        } else { 
+            playerScore++;
+        } else {
             log = " It's a tie";
         }
     } else if (playChoice === "paper") {
         if (computerChoice === "rock") {
             log = " Yayy! you win, Paper beat Rock";
+            playerScore++;
         } else if ( computerChoice === "scissors") {
             log = 'Sorry, you lose! Scissors beats Paper';
+            computerScore++;
         } else {
             log = " It's is a tie";
         }
     } else {
         if (computerChoice === "rock") {
             log = " Sorry, you lose! Rock beats scissors";
+            computerScore++;
         } else if ( computerChoice === "paper") {
             log = " Yay, you win! Scissors beat paper";
+            playerScore++;
         } else {
             log = " it's a tie";
         }
@@ -50,4 +60,11 @@ function playRound (playChoice, computerChoice) {
 }
 const result = playRound(playChoice, computerChoice);
 console.log(result);
-
+console.log("Your score is", playerScore);
+console.log("Computer score is", computerScore);
+if (playerScore === 5 || computerScore === 5) {
+    if (playerScore === 5) { console.log("You win");}
+    else { console.log("you lost");}
+    break;
+}
+}
