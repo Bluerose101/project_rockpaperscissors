7a3d0a86f508faf7317e7570886284f94c6ce994
Bluerose101 <thanhhong10194@gmail.com>
2022-06-24 08:12:03 -0400

    css and html

---
+++ app.js
@@ -0,0 +1,10 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("comp-score");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+
